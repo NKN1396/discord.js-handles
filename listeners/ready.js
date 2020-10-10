0964b93d85259eab786e2e5e3840547bcc5f9694
NKN1396 <someone.took.nkn1396@gmail.com>
2020-10-10 17:02:13 +0200

    Update ready.js

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,24 +1,19 @@
-var moment = require("moment")
+var { DateTime } = require("luxon")
 
 module.exports = function(client, options){
-
-	console.log(`${options.name} has been started on ${moment(Date.now()).utcOffset(1).format("LLLL")}`)
-
+	let time_now = DateTime.utc()
+	console.log(`${options.name} has been started on ${time_now.toLocaleString(DateTime.DATE_HUGE)} at ${time_now.toLocaleString(DateTime.TIME_24_SIMPLE)}`)
 	client.on("ready", function() {
 		console.log("Logged in as " + client.user.tag);
 
-		//Notify users and channels that the bot has been restarted.
+		//Notify all users and channels that the bot has been restarted.
 		(async () => {
 			for(var user of options.notify.users)
 				await client.users.get(user).send(`✅ ${options.name} has been restarted.`)
-					.catch(e => {
-						console.error(e)
-					})
+					.catch(console.error)
 			for(var channel of options.notify.channels)
 				await client.channels.get(channel).send(`✅ ${options.name} has been restarted.`)
-					.catch(e => {
-						console.error(e)
-					})
+					.catch(console.error)
 		})()
 		
 	})
